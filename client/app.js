18e595c641c47696209cc8c4398ddb3da2447c2a
Marcin Lech <arcinech@gmail.com>
2022-07-14 12:50:31 +0200

    Add: socet io to app client

--- client/app.js
+++ client/app.js
@@ -8,8 +8,11 @@ const messagesList = document.getElementById('messages-list');
 const addMessagesForm = document.getElementById('add-messages-form');
 const messageContentInput = document.getElementById('message-content');
 
+const socket = io();
+
 //functions
 const addMessage = (user, message) => {
+  console.log(user);
   const li = document.createElement('li');
   li.classList.add('message');
   li.classList.add('message--received');
@@ -22,6 +25,9 @@ const addMessage = (user, message) => {
   messagesList.appendChild(li);
 }
 
+//socket listeners
+socket.on('message', ({author, message}) => addMessage(author, message));
+
 // Event listeners
 loginForm.addEventListener('submit', e => {
   e.preventDefault();
@@ -29,14 +35,16 @@ loginForm.addEventListener('submit', e => {
   const loginInput = document.getElementById('username').value;
   if(!loginInput) return alert('Please enter username');
 
-  userName = loginInput.value;
+  userName = loginInput;
   loginForm.classList.remove('show');
   messagesSection.classList.add('show');
 });
 
 addMessagesForm.addEventListener('submit', e => { 
   e.preventDefault();
-  console.log(messageContentInput);
-  if(!messageContentInput.value) return alert('Please write message');
+  let messageContent = messageContentInput.value;
+  if(!messageContent.length) return alert('Please write message');
   addMessage(userName, messageContentInput.value);
-})
+  socket.emit('message', {author: userName, message: messageContent});
+  messageContentInput.value = '';
+});
