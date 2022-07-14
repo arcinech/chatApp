41cc577b946eb590e76aad5ab191da5bd782b5e1
Marcin Lech <arcinech@gmail.com>
2022-07-14 12:51:00 +0200

    Add: configure socet for communication

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const path = require('path');
 //Initial table of messages
-const messages = require('./messages');
+const messages = require('./messages.js');
+const socket = require('socket.io');
 
 const app = express();
+
+// Listen on port 8000 or process.env.port
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
+});
+
+const io=socket(server);
 
 //Run client on express server
 app.use(express.static(path.join(__dirname, '/client/')));
@@ -16,7 +24,13 @@ app.use((req, res) => {
   res.status(404).json({message: 'Not found...' });
 });
 
-// Listen on port 8000 or process.env.port
-app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running on port: 8000');
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('message', (message) =>
+   { 
+    console.log('Oh, I\'ve got something from ' + socket.id);
+    socket.broadcast.emit('message', message);
+    // console.log(messages);
+    });
+  console.log('I\'ve added a listener on message event \n');
 });
